a465b4901f0f29d83ebb1f0a79cce033fe163bcc
Justijn Depover <justijndepover@gmail.com>
2020-11-16 14:30:14 +0100

    git functions

--- system/alias.sh
+++ system/alias.sh
@@ -36,3 +36,22 @@ docker-deamon(){
         echo "usage:\ndocker-deamon start: start the Docker application\ndocker-deamon stop: stop the Docker application"
     fi
 }
+
+# Git
+g:push(){
+    changed=0
+    git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+    if [ $changed = 1 ]; then
+        echo "Your branch is behind on remote";
+    else
+        git add .
+        git commit -m $1
+        git push
+    fi
+}
+
+g:fix(){
+    git add .
+    git commit -m '🐞 quick bugfix'
+    git push
+}
