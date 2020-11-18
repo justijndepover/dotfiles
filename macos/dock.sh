6bf7bf41abd7f9ee26128aaab2c79366dbe46ef1
Justijn Depover <justijndepover@gmail.com>
2020-11-18 21:06:40 +0100

    dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,7 +3,7 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
-dockutil --no-restart --add "/Applications/Atom.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 
 killall Dock
